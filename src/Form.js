9fef17047bd6b562558eda2338c0ef940eba5809
Omolewu Sunday <omolewu12@gmail.com>
2022-05-29 13:21:18 +0100

    State errors rectified

--- src/Form.js
+++ src/Form.js
@@ -1,64 +1,92 @@
 import { useState, useEffect } from 'react';
 const Form = () => {
     const [data, setData] = useState(0);
-    const [User, setUser] = useState({
+    const [user, setUser] = useState({
         firstName: "",
         lastName: "",
         userName: "",
-        errors: {
-            lastNameErr: " ",
-            firstNameErr: " First Name can not be blank",
-            userNameErr: "",
-        },
+        // errors: {
+        //     lastNameErr: " ",
+        //     firstNameErr: "",
+        //     userNameErr: "",
+        // },
     })
+
+    const [errors, setErrors] = useState({
+        lastNameErr: " ",
+        firstNameErr: "",
+        userNameErr: "",
+    });
 
     const ChangeData = (e) => {
         let value = e.target.value;
         let name = e.target.name
         setUser({
-            ...User,
+            ...user,
             [name]: value,
         })
 
     }
-
     function submitData(event) {
         event.preventDefault();
+        if (!user.lastName) {
+            setErrors(errors => {
+                return {
+                    ...errors,
+                    lastNameErr: " Your Last name is required",
+                }
+            })
+        }
+        if (!user.firstName) {
+            // console.log("First Name is required")
+            setErrors(errors => {
+                return {
+                    ...errors,
+                    firstNameErr: " Your First name is required",
+                }
+            })
+        }
 
+        if (!user.userName) {
+            setErrors(errors => {
+                return {
+                    ...errors,
+                    userNameErr: "User name is required",
+                }
+            })
+        }
     }
-
-
     // useEffect(() => {
     //     setData('React is not difficult once you understand Js')
     // }, [])
     return (
         <>
             <div>
-                {/* <p>First Name : {User.firstName}</p>
-                <p>Last Name : {User.lastName}</p> */}
+                {/* <p>First Name : {user.firstName}</p>
+                <p>Last Name : {user.lastName}</p> */}
             </div>
             <form onSubmit={submitData} className="row g-3 needs-validation" noValidate>
                 <div className="col-md-4">
                     <label htmlFor="validationCustom01" className="form-label">First name</label>
-                    <input name="firstName" onChange={ChangeData} type="text" className="form-control" id="validationCustom01" defaultValue="Mark" />
-                    {User.errors.firstNameErr}
+                    <input name="firstName" onChange={ChangeData} type="text" className="form-control" id="validationCustom01" />
+                    {errors.firstNameErr}
                 </div>
                 <div className="col-md-4">
                     <label htmlFor="validationCustom02" className="form-label">Last name</label>
                     <input name="lastName" type="text"
                         onChange={ChangeData}
-                        className="form-control" id="validationCustom02" defaultValue="Otto" />
+                        className="form-control" id="validationCustom02" />
                     <div className="valid-feedback">
                         Looks good!
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <label htmlFor="validationCustomUsername" className="form-label">Username</label>
+                    <label htmlFor="validationCustomusername" className="form-label">username</label>
                     <div className="input-group">
                         <span className="input-group-text" id="inputGroupPrepend">@</span>
                         <input name="userName"
                             onChange={ChangeData}
-                            type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
+                            type="text" className="form-control" id="validationCustomusername" aria-describedby="inputGroupPrepend" />
                         <div className="invalid-feedback">
                             Please choose a username.
                         </div>
@@ -72,8 +100,8 @@ const Form = () => {
             {/* <form className="row g-3 needs-validation" noValidate>
                 <div className="col-md-4">
                     <label htmlFor="validationCustom01" className="form-label">First name</label>
-                    <input name="firstName" onChange={(e) => setUser({
-                        ...User,
+                    <input name="firstName" onChange={(e) => setuser({
+                        ...user,
                         firstName: e.target.value,
                     })} type="text" className="form-control" id="validationCustom01" defaultValue="Mark" />
 
@@ -84,8 +112,8 @@ const Form = () => {
                 <div className="col-md-4">
                     <label htmlFor="validationCustom02" className="form-label">Last name</label>
                     <input type="text"
-                        onChange={(e) => setUser({
-                            ...User,
+                        onChange={(e) => setuser({
+                            ...user,
                             lastName: e.target.value,
                         })}
                         className="form-control" id="validationCustom02" defaultValue="Otto" />
@@ -94,12 +122,12 @@ const Form = () => {
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <label htmlFor="validationCustomUsername" className="form-label">Username</label>
+                    <label htmlFor="validationCustomusername" className="form-label">username</label>
                     <div className="input-group">
                         <span className="input-group-text" id="inputGroupPrepend">@</span>
                         <input
                             onChange={}
-                            type="text" className="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required />
+                            type="text" className="form-control" id="validationCustomusername" aria-describedby="inputGroupPrepend" required />
                         <div className="invalid-feedback">
                             Please choose a username.
                         </div>
